751979a03791da7dd25b91df0125648612fd9598
BURAK DÜLGEROĞLU <dulgerogluburak04@gmail.com>
2025-05-23 16:34:06 +0700

    Update Car.cs

--- Encapsulation/Car.cs
+++ Encapsulation/Car.cs
@@ -1,46 +1,46 @@
 ﻿using System;
 namespace Encapsulation
 {
-	public class Car //class
-	{
+    public class Car //class
+    {
         //fields
-        private string _brand; 
-		private string _model;
-		private string _colour;
-		private int _doorNumber;
+        private string _brand;
+        private string _model;
+        private string _colour;
+        private int _doorNumber;
 
-		//overloaded constructor
-		public Car(string brand, string model, string colour, int doorNumber)
-		{
-			Brand = brand;
-			Model = model;
-			Colour = colour;
-			DoorNumber = doorNumber;
-		}
+        //overloaded constructor
+        public Car(string brand, string model, string colour, int doorNumber)
+        {
+            Brand = brand;
+            Model = model;
+            Colour = colour;
+            DoorNumber = doorNumber;
+        }
 
-		public string Brand //property
-		{
-			get { return _brand; }
+        public string Brand //property
+        {
+            get { return _brand; }
 
-			set
-			{
-				if(!string.IsNullOrWhiteSpace(value))
-				{
-					_brand = value;
-				}
-				else
-				{
-					Console.WriteLine("Marka ismi boş geçilemez.");
-				}
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _brand = value;
+                }
+                else
+                {
+                    Console.WriteLine("Marka ismi boş geçilemez.");
+                }
 
-			}
-		}
+            }
+        }
 
-		public string Model
-		{
-			get { return _model; }
-			set
-			{
+        public string Model
+        {
+            get { return _model; }
+            set
+            {
                 if (!string.IsNullOrWhiteSpace(value))
                 {
                     _model = value;
@@ -50,37 +50,37 @@ namespace Encapsulation
                     Console.WriteLine("Model ismi boş geçilemez.");
                 }
             }
-		}
+        }
 
-		public string Colour
-		{
-			get { return _colour; }
-			set { _colour = value; }
-		}
+        public string Colour
+        {
+            get { return _colour; }
+            set { _colour = value; }
+        }
 
-		public int DoorNumber
-		{
-			get { return _doorNumber; }
-			set
-			{
-				if(value == 2 || value == 4)
-				{
-					_doorNumber = value;
-				}
-				else
-				{
-					Console.WriteLine("Geçersiz kapı sayısı girildi.");
-					_doorNumber = -1;
-				}
-			}
-		}
+        public int DoorNumber
+        {
+            get { return _doorNumber; }
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _doorNumber = value;
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz kapı sayısı girildi.");
+                    _doorNumber = -1;
+                }
+            }
+        }
 
 
-		public void ShowInfo() // bilgileri ekrana yazdıran metot
-		{
-			Console.WriteLine($"Araba markası: {Brand}, modeli: {Model},  rengi: {Colour}, kapı sayısı: {DoorNumber}");
-		}
+        public void ShowInfo() // bilgileri ekrana yazdıran metot
+        {
+            Console.WriteLine($"Araba markası: {Brand}, modeli: {Model},  rengi: {Colour}, kapı sayısı: {DoorNumber}");
+        }
 
-	}
+    }
 }
 
